76f27d66e8ca23899233374b7bc7f138aad2dd17
Rhys Evans <rhys.evans@ft.com>
2013-10-28 10:28:37 +0000

    added get to verify new verbose ids work

--- test/toc.test.js
+++ test/toc.test.js
@@ -31,6 +31,34 @@ suite('toc', function() {
       assert.equal($('.toc ul').length, 1);
     });
   });
+  
+  suite('anchor id formatting', function() {
+    test('should be able to use verbose ids', function() {
+      assert.equal($('.toc ul a').length, 0);
+
+      $('.toc').toc({
+        container: '#fixture',
+        anchorName: 'verbose'
+      });
+
+      assert.equal($('#fixture').find('h1').prev().attr('id'), 'toc-page-title');
+      assert.equal($('#fixture').find('h2').eq(1).prev().attr('id'), 'toc-sub-heading-2');
+    });
+
+   
+    // test('should be able to use custom id generator', function(done) {
+    //   assert.equal($(window).scrollTop(), 0);
+      
+    //   $('.toc').toc({
+    //     container: '#fixture',
+    //     anchorName: function (i, heading, prefix) {
+    //       return i + prefix;
+    //     }
+    //   });
+
+    //   assert.equal($('h1').prev().attr('id'), '0toc');
+    // });
+  });
 
   suite('toc navigation', function() {
     test('should contain links', function() {
@@ -76,4 +104,5 @@ suite('toc', function() {
       }, 110);
     });
   });
+  
 });
